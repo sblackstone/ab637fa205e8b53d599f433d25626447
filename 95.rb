8835df1a48605a6bb24e341830f0a2d90d8775d1
Stephen Blackstone <sblackstone@gmail.com>
2013-03-20 16:48:43 -0400

    95

--- 95.rb
+++ 95.rb
@@ -2,7 +2,7 @@ require 'pp'
 
 SIZE = 1_000_000
 
-@sdiv = Hash.new(0)
+@sdiv = Array.new(SIZE,0)
 
 1.upto(SIZE) do |d|
   i = d*2
@@ -12,6 +12,38 @@ SIZE = 1_000_000
   end
 end
 
+
+@max_length = 0
+@max_i = 0
+@min = 0
+1.upto(SIZE-1) do |i|
+  h = Hash.new(false)
+  j = i
+  while !h[j]
+    begin
+
+    h[j] = true
+    j = @sdiv[j]
+    break if j > 1_000_000
+    rescue
+      pp i
+      pp j
+    end
+  end
+  if j == i && h.keys.size > @max_length
+    @max_length = h.keys.size
+    @max_i = i   
+    @min = h.keys.min
+  end
+end
+
+pp @max_length
+pp @max_i
+pp @min
+=begin
+puts @sdiv[12496]
+
+exit
 def chain(n)
   hsh = Hash.new(false)
   hsh[n] = true
@@ -27,8 +59,13 @@ def chain(n)
 end
 
 
+pp chain(12496)
+exit
+
+
 0.upto(1_000_000) do |i|
   puts "I: #{i}"
   pp chain(i)
 end
 
+=end

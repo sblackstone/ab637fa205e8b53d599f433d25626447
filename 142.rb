0a0dbe08799e7af3b56c6e0bf43b536b3b7a3ae2
Stephen Blackstone <sblackstone@gmail.com>
2013-03-25 15:20:55 -0400

    142

---
+++ 142.rb
@@ -0,0 +1,46 @@
+=begin
+Find the smallest x + y + z with integers x > y > z > 0 such that x + y, x - y,   
+                                                                  x + z, x - z,   
+                                                                  y + z, y - z are all perfect squares.
+
+x + y is square
+x - y is square
+
+
+=end
+require 'pp'
+
+@track = Hash.new
+
+
+@squares = Hash.new(false)
+0.upto(1000) do |i|
+  @squares[i**2] = true
+end
+
+
+1.upto(1000) do |i|
+  (i+1).upto(1000) do |j|
+    s1 = i**2
+    s2 = j**2
+    diff = s2 - s1
+    if (diff) % 2 == 0
+      @track[s2 - (diff/2)] ||= Array.new
+      @track[s2 - (diff/2)].push(diff/2)
+      #puts "#{s2 - (diff/2)} +- #{diff/2} is a perfect square (#{j} and #{i})"
+    end
+    
+  end
+end
+
+@track.each do |x,v|
+  v.combination(2) do |a|
+    y = a.max
+    z = a.min
+    if @squares[y+z] and @squares[y-z]
+      puts "#{x} #{y} #{z} #{x+y+z}"
+
+    end
+  end
+  
+end

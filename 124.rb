dc8d1df0935857e7ab7d51816cc057ad2f86bee7
Stephen Blackstone <sblackstone@gmail.com>
2013-05-10 14:35:09 -0400

    124

--- 124.rb
+++ 124.rb
@@ -1,20 +1,37 @@
+=begin
+The radical of n, rad(n), is the product of distinct prime factors of n. For example, 504 = 23 × 32 × 7, so rad(504) = 2 × 3 × 7 = 42.
+
+If we calculate rad(n) for 1 ≤ n ≤ 10, then sort them on rad(n), and sorting on n if the radical values are equal, we get:
+
+Let E(k) be the kth element in the sorted n column; for example, E(4) = 8 and E(6) = 9.
+
+If rad(n) is sorted for 1 ≤ n ≤ 100000, find E(10000).
+
+=end
+
 require './primes.rb'
 
-MAX = 100
-Primes.setup(100)
+MAX = 100_000
+Primes.setup(100_000)
 
 @track = Hash.new(1)
 
-
+@track[1] = 1
 Primes.primes.each do |p|
   k = 1
-  while k*p < MAX
+  while k*p <= MAX
     @track[k*p] *= p 
     k += 1
   end
 end
 
-@sort = @track.sort_by {|k,v| v }
+=begin
+1.upto(MAX) do |i|
+  puts "#{i}: #{@track[i]}"
+end
+=end
 
-pp @sort[4]
-pp @sort[8]
+@stuff =  @track.sort_by {|k,v|  [v,k] }
+
+
+pp @stuff[10000-1]

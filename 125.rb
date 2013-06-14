bf608c67809b6da90027984b6efec02cdec4d2f3
Stephen Blackstone <sblackstone@gmail.com>
2013-06-14 10:08:15 -0400

    125

---
+++ 125.rb
@@ -0,0 +1,33 @@
+=begin
+The palindromic number 595 is interesting because it can be written as the sum of consecutive squares: 62 + 72 + 82 + 92 + 102 + 112 + 122.
+
+There are exactly eleven palindromes below one-thousand that can be written as consecutive square sums, and the sum of these palindromes is 4164. Note that 1 = 02 + 12 has not been included as this problem is concerned with the squares of positive integers.
+
+Find the sum of all the numbers less than 108 that are both palindromic and can be written as the sum of consecutive squares.
+=end
+
+require 'pp'
+
+@f = Array.new
+
+def sq(n)
+ (2*n**3 + 3*n**2 + n) / 6
+end
+
+@h = Hash.new
+
+1.upto(10000) do |i|
+  (i+1).upto(10000) do |j|
+    diff = sq(j) - sq(i-1)
+    if diff < 10**8
+      if diff.to_s == diff.to_s.reverse
+        puts "#{diff}: #{i} to #{j}"
+        @h[diff] = true 
+      end
+    else
+      break
+    end
+  end  
+end
+
+puts @h.keys.inject(&:+)

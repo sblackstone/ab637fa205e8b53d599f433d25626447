b821e4f8b0bb97ec9db33397ee1e5b3eaa6431cd
Stephen Blackstone <sblackstone@gmail.com>
2013-11-12 17:42:35 -0500

    82

--- 82.rb
+++ 82.rb
@@ -1,119 +1,149 @@
-require 'pp'
+=begin
+
+131,673,234,103,18
+201,96,342,965,150
+630,803,746,422,111
+537,699,497,121,956
+805,732,524,37,331
+
+=end
+
 require 'csv'
+require 'pp'
 
-WIDTH  = 80
-HEIGHT = 80
-START = WIDTH+3
-GOAL  = START+WIDTH-1
+class Node
+  attr_reader :value, :row, :col, :hash
 
-@f_score = Hash.new
-@g_score = Hash.new
-@closedset = Hash.new(false)
-@openset   = Hash.new(false)
-@came_from = Hash.new(nil)
+  attr_accessor :neighbors, :up, :down, :left, :right
 
-@h_cache = Hash.new(nil)
-
-def h(s,goal)
-  @h_cache[s*10000+goal] ||= calc_h3(s,goal)
-end
-
-def calc_h3(s,goal)
-  return 0 if s == goal
-  unless @matrix[s-WIDTH-1].nil?
-    lowest = choose_best([ s+1, s-WIDTH-2])
-    return @matrix[lowest] + @matrix[s-WIDTH-2] + h(s-WIDTH-1, goal) 
+  def ==(other)
+    self.hash == other.hash
   end
-  return @matrix[s-WIDTH-2] + h(s-WIDTH-2, goal) unless @matrix[s-WIDTH-2].nil?
-  return @matrix[s+1] + h(s+1, goal)
-end
 
-def choose_best(set)
-  best = set.first
-  set.each do |os|
-    if @matrix[os] < @matrix[best]
-      best = os
-    end
+  alias eql? == 
+
+
+
+  def to_s
+    "(#{@row},#{@col})"
   end
-  return best
-end
+  
+    
+  def initialize(value, row, col)
+    @value = value
+    @neighbors = Array.new
+    @row   = row
+    @col   = col
+    @hash  = row * 10000 + col
 
-def choose_current
-  choose_best(@openset.keys)
-end
-
-
-def neighbor_nodes(s)
-  nodes = Array.new
-  nodes << (s+1) unless @matrix[s+1].nil?
-  nodes << (s+WIDTH+2) unless @matrix[s+WIDTH+2].nil?
-  nodes << (s-WIDTH-2) unless @matrix[s-WIDTH-2].nil?
-  return nodes  
-end
-
-def reconstruct_path(came_from, current_node)
-  #puts @matrix[current_node]
-  p = 0
-  if came_from[current_node]
-    p = reconstruct_path(came_from, came_from[current_node])
-    return(p+@matrix[current_node])
-  else
-    return @matrix[current_node]
+    @up    = nil
+    @down  = nil
+    @left  = nil
+    @right = nil
   end
+  
 end
-
-
-def a_star(start, goal)
-  @g_score[start] = @matrix[start]  
-  @f_score[start] = @g_score[start] + h(start,goal)
-  @openset[start] = true
-  while !@d.empty?
-    current = choose_current
-    if (current == goal)
-      return reconstruct_path(@came_from, current)
-    end
-    @openset.delete(current)
-    @closedset[current] = true
-    @neighbors = neighbor_nodes(current)
-    @neighbors.each do |neighbor|
-      next if @closedset[neighbor]
-      tentative_g = @g_score[current] + @matrix[neighbor]
-      if !@openset[neighbor] or tentative_g < @g_score[neighbor]
-        @openset[neighbor] = true
-        @came_from[neighbor] = current
-        @g_score[neighbor] = tentative_g
-        @f_score[neighbor] = @g_score[neighbor] + h(neighbor,goal)
-      end
-    end    
-  end
-  return false
-end
-
 
 @matrix = Array.new
-@matrix << Array.new(WIDTH+2, nil)
+row_num = 0
 CSV.foreach("./81-matrix.txt") do |row|
- r = row.map {|x| x.to_i }
- r.unshift(nil)
- r.push(nil)
- @matrix << r
-end
-@matrix << Array.new(WIDTH+2, nil)
-@matrix.flatten!
+ col_num = 0
+ row_a = Array.new
 
-@best = 9999999999
-@start = START
-
-while !@matrix[@start].nil?
-  @f_score = Hash.new
-  @g_score = Hash.new
-  @closedset = Hash.new(false)
-  @openset   = Hash.new(false)
-  @came_from = Hash.new(nil)
-  puts "start = #{@start} #{@matrix[@start]}"
-  pp a_star(@start, GOAL)
-  @start += WIDTH+2
-  puts "---"
+ row.each do |x|
+   row_a.push Node.new(x.to_i, row_num, col_num)
+   col_num += 1
+ end
+ 
+ @matrix << row_a
+ row_num += 1
 end
 
+0.upto(@matrix.length-1) do |row|
+  0.upto(@matrix[0].length-1) do |col|
+      @matrix[row][col].neighbors.push   @matrix[row+1][col] unless row == @matrix.length-1
+      @matrix[row][col].down =           @matrix[row+1][col] unless row == @matrix.length-1    
+      @matrix[row][col].neighbors.push   @matrix[row-1][col] unless row == 0
+      @matrix[row][col].up =             @matrix[row-1][col] unless row == 0
+      @matrix[row][col].neighbors.push   @matrix[row][col+1] unless col == @matrix[0].length - 1
+      @matrix[row][col].right =          @matrix[row][col+1] unless col == @matrix[0].length - 1
+      #@matrix[row][col].neighbors.push  @matrix[row][col-1] unless col == 0
+      #@matrix[row][col].left =           @matrix[row][col-1]   unless col == 0
+  end  
+end
+
+@h_cache = Hash.new(false)
+
+@matrix.flatten.each do |n|
+  @h_cache[n] = (@matrix.length-1 - n.col).abs
+end
+
+
+def h(node)
+  @h_cache[node]
+end
+
+
+def a_star(start)
+  closedset = Hash.new(false)
+  openset   = Hash.new(false)
+  openset[start] = 0
+  came_from = Hash.new
+  g_scores  = Hash.new
+  f_scores  = Hash.new  
+  g_scores[start] = 0
+  f_scores[start] = g_scores[start] + h(start)
+  until openset.empty?
+    current = openset.keys.first    
+    #puts openset.size
+    openset.keys.each do |os|
+      if f_scores[os] < f_scores[current]
+        current = os
+      end
+    end
+    return came_from,current if current.col ==  @matrix.length-1
+
+    #puts "Expanding #{current}"
+    # Current is now the item from open set with the lowest f_score..
+    openset.delete current    
+    closedset[current] = true
+    current.neighbors.each do |neighbor|
+      t_g_score = g_scores[current] + neighbor.value
+      t_f_score = t_g_score + h(neighbor)
+      if closedset[neighbor] and t_f_score >= f_scores[neighbor]
+        next 
+      end
+      if !openset.include? neighbor or t_f_score < f_scores[neighbor]
+        came_from[neighbor] = current
+        g_scores[neighbor] = t_g_score
+        f_scores[neighbor] = t_f_score
+        openset[neighbor] = true
+      end
+    end
+  end
+  came_from
+end
+
+def reconstruct_path(came_from, current)
+  if came_from.keys.include? current
+    p = reconstruct_path(came_from, came_from[current])
+    return [ p, current].flatten
+  else
+    return [ current ]
+  end
+end
+
+puts "Starting A*"
+
+min = 9999999999
+0.upto(79) do |i|
+  start = @matrix[i][0]
+  cf,g = a_star(start)
+  path = reconstruct_path(cf ,  g )
+  v = path.map {|x| x.value}.inject(&:+)    
+  min = v if v < min
+  puts "#{i}: #{v}"
+end
+
+puts "Min: #{min}"
 

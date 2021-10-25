0146a384c8b7cee5f239d162bc0288cff6668359
Stephen Blackstone <sblackstone@gmail.com>
2021-10-25 12:34:24 -0400

    closer on 684

--- 684.js
+++ 684.js
@@ -1,4 +1,56 @@
 
+const modulus = 1000000007;
+
+/*
+def modpow(b,e,m)
+  result = 1
+  while e > 0
+    if (e & 1) == 1
+      result = (result * b) % m;
+    end
+    e = e >> 1;
+    b = (b * b) % m;
+  end
+  return result
+end
+*/
+
+const modPow = (b, e, m) => {
+  let result = 1;
+
+  b = b % m;
+
+  if (b == 0) {
+    return 0;
+  }
+
+  while (e > 0) {
+    if (e & 1 == 1) {
+      result = (result * b) % m;
+    }
+    e = e >> 1;
+    b = (b*b) % m;
+  }
+  return result;
+}
+
+
+for (let i = 0; i < 100; i++) {
+  for (let j = 0; j < 100; j++) {
+    for (let k = 1; k < 100; k++) {
+      let a = modPow(i,j,k);
+      let b = (i**j) % k;
+      if (a !== b) {
+        console.log(i,j,k,a,b);
+      }
+    }
+  }
+}
+
+console.log(modPow(2, 5, 3));
+process.exit(-1);
+
+
 const digitSum = (x) => {
   let result = 0;
   while (x > 0) {
@@ -16,22 +68,37 @@ const fibSeq = (count) => {
   return result;
 }
 
+const ninesCache = {};
+
+const nineContribution = (nineLength) => {
+  if (nineLength === 0) {
+    return 0
+  };
+  if (nineLength === 1) {
+    return 9;
+  }
+  if (ninesCache[nineLength] > 0) {
+    return ninesCache[nineLength];
+  }
+  ninesCache[nineLength] = 9 * 10**(nineLength - 1) + nineContribution(nineLength - 1);
+  return ninesCache[nineLength];
+}
+
+
+for (let i = 0; i < 1000000; i++) {
+    nineContribution(i);
+}
+
 const calcFast = (x) => {
   if (x < 10) {
     return x;
   }
-  let tenPower = 1;
   let nineLength = Math.floor(x / 9);
-
-  let result = x - (nineLength * 9);
-  result = result * 10**(nineLength);
-  for (let i = 0; i < nineLength; i++) {
-    result +=  (tenPower * 9);
-    tenPower *= 10;
-  }
+  let result = (x - (nineLength * 9))*10**(nineLength) + nineContribution(nineLength);
   return result;
 
 };
+
 
 
 
--- 684.rb
+++ 684.rb
@@ -1,26 +1,46 @@
-
+MOD = 1000000007
 
 def calcFast(x)
   return x if x < 10
-  tenPower = 1
   nineLength = (x / 9).floor
-  result = x - (nineLength * 9)
-  result = result * 10**nineLength
-  0.upto(nineLength - 1) do |i|
-    result += tenPower * 9
-    tenPower *= 10
-  end
-  return result
+  leadingDigit = x - (nineLength * 9)
+  leadingTerm = (leadingDigit * 10.pow(nineLength))
+  ninesTerm   = (10.pow(nineLength) - 1)
+  return (leadingTerm + ninesTerm)
 end
 
+def calcFastMod(x, m)
+  return x if x < 10
+  nineLength = (x / 9).floor
+  leadingDigit = x - (nineLength * 9)
+  leadingTerm = (leadingDigit * 10.pow(nineLength, m)) % m
+  ninesTerm   = (10.pow(nineLength, m) - 1) % m
+  return (leadingTerm + ninesTerm) % m
+end
 
-def fibSeq
+testSum = ((1..20).map {|x| calcFast(x)}.inject(:+)) % MOD
+
+puts "Expected = #{1074 % MOD} got #{testSum}"
+
+if testSum != 1074 % MOD
+  raise "Bad test"
+end
+
+def linear
+  i = 1
+  while i < 21
+    yield i, i
+    i += 1
+  end
+end
+
+def fib
   i = 2
   x0 = 0
   x1 = 1
-  yield x0,0
-  yield x1,1
-  while true
+  #yield x0,0
+  #yield x1,1
+  while i <= 90
     x2 = x0 + x1
     yield x2, i
     x0 = x1
@@ -29,23 +49,14 @@ def fibSeq
   end
 end
 
-0.upto(100) do |i|
-  puts "#{i} #{calcFast(i)}"
-end
-
-
-
 sum = 0
-fibSeq do |fib, i|
-  if i > 90
-    break
-  end
+sum2 = 0
 
-  if i > 1
-    puts "Calculating #{i}"
-    sum += calcFast(i)
-  end
-  puts "#{i} #{fib}"
+fib do |fib, i|
+  #puts "#{i}: val = #{fib} value = #{calcFast(fib)}"
+  #sum = (sum + calcFast(fib))
+  sum2 = sum2 + calcFastMod(fib, MOD)
 end
 
-puts sum
+puts sum % MOD
+puts sum2 % MOD
